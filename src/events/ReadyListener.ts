68a18ab8f7cf3e43a8a6fecfa3e2ce23325672dc
jvopinho <jvopinho.dev@gmail.com>
2022-07-19 18:09:29 -0300

    Adding `ReadyListener`

---
+++ src/events/ReadyListener.ts
@@ -0,0 +1,15 @@
+import Eris from 'eris';
+
+import EventListener from '../structures/EventListener';
+
+class RawWSListener extends EventListener {
+    constructor(client: LunaryClient) {
+        super(client, 'ready');
+    }
+
+    public async run() {
+        logger.info(`Bot is running on port ${this.client.user.username}#${this.client.user.discriminator} (ID: ${this.client.user.id})`, { label: 'Lunary' });
+    }
+}
+
+export default RawWSListener;
